ca4aba7059442ef0f7c0b777f6c7d0487173c679
Biack1st <98064246+Biack1st@users.noreply.github.com>
2022-11-05 19:27:56 -0500

    added more settings

--- src/BlackChat/BlackChatCore/config.lua
+++ src/BlackChat/BlackChatCore/config.lua
@@ -1,7 +1,9 @@
 return {
 	PrimaryColor = Color3.fromRGB(0, 0, 0), -- primary color for the board
-	ItemTransparency = 0.5, -- item transparency for the board
+	ItemTransparency = 0.25, -- item transparency for the board
 	FontColor = Color3.fromRGB(255, 255, 255), -- font color
+	Font = Enum.Font.GothamMedium, -- font
+	TextSize = 20, -- text size for the text
 	ColorPattern = {}, -- for color patterns so like red, white, black and will format it inside of the coloring. Overrides primary color
 	GroupIcons = {
 		-- icons for certain groups ie admins and star creators.
