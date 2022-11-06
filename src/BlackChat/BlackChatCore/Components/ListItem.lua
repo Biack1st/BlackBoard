c90dda7a708163b4a227504aff011ddbfbc73cf8
Biack1st <98064246+Biack1st@users.noreply.github.com>
2022-11-05 19:28:04 -0500

    added more stuff

--- src/BlackChat/BlackChatCore/Components/ListItem.lua
+++ src/BlackChat/BlackChatCore/Components/ListItem.lua
@@ -6,9 +6,23 @@ local ListItem = Roact.Component:extend("ListItem")
 function ListItem:render(props)
 	return (
 		Roact.createElement("Frame", {
-			Size = UDim2.new(1, 0, 0, 45),
+			Size = UDim2.new(0, 200, 0, 45),
 			BackgroundTransparency = config.ItemTransparency,
 			BackgroundColor3 = config.PrimaryColor,
+		}, {
+			PlayerName = Roact.createElement("TextLabel", {
+				Size = UDim2.fromScale(0.6, 1),
+				TextColor3 = config.FontColor,
+				Font = config.Font,
+				BackgroundTransparency = 1,
+				TextSize = config.TextSize,
+			}),
+			["$layout"] = Roact.createElement("UIListLayout", {
+				SortOrder = Enum.SortOrder.LayoutOrder,
+				FillDirection = Enum.FillDirection.Horizontal,
+				VerticalAlignment = Enum.VerticalAlignment.Center,
+				HorizontalAlignment = Enum.HorizontalAlignment.Center,
+			}),
 		})
 	)
 end
